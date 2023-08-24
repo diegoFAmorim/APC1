a1ed19f3ef52a7407f60c366c2e1d69633f0c1fc
diegoFAmorim <25529757-diegoFAmorim@users.noreply.replit.com>
2023-08-24 00:46:09 +0000

    arquivos da aula 1

---
+++ aulas/aula01/meuprogama.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+
+// pra comentar só colocar barra duas vezes
+int main() {
+  printf("Ola pessoas lindas!\n");
+  printf("A raiz quadrada de 2 é %f\n",sqrt(2));
+  
+  return 0;
+}
